65fb781df4e8f5efdcceb2e4bea0b3c61e667f0c
brijkaur <brajenderkaur@gmail.com>
2020-04-10 11:58:37 +0530

    commiting Source Control files

---
+++ retailbank-app/src/main/java/com/retailbanking/app/App.java
@@ -0,0 +1,13 @@
+package com.retailbanking.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Retail Banking Application" );
+    }
+}
